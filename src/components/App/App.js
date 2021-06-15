5357553468d62fc4729ac5898b84bf6bec254472
Соломон Захаров <solomon7and@gmail.com>
2021-06-15 11:29:49 +0500

    Минимальная стилистика

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ItemList from "../ItemList/ItemList";
 import InputItem from "../InputItem/InputItem";
 import Footer from "../Footer/Footer";
+import './App.css';
 
 const items = [
   {
@@ -16,11 +17,11 @@ const items = [
 ]
 
 const App = () => (
-  <div>
-    <h1>Важные дела:</h1>
+  <div className="wrap">
+    <h1 className="wrap__title">Важные дела:</h1>
     <InputItem />
     <ItemList items={items} />
-    <Footer count={3}/>
+    <Footer className="wrap__Footer" count={3}/>
   </div>
 )
 
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Item = ({value}) => (<span>{value}</span>);
+const Item = ({value}) => (<span className="wrap__ItemList">{value}</span>);
 
 export default Item;
