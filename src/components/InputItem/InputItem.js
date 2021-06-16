893ac03ca3c89b5e6d963db491b3a85f5979a4cf
Соломон Захаров <solomon7and@gmail.com>
2021-06-16 16:53:14 +0500

    Material UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from '../App/App.module.css'
+import TextField from '@material-ui/core/TextField';
+
 
 const InputItem = () => (
-    <input className={styles.InputItem} type="text" placeholder="Что нужно выполнить?"/>
+    <div >
+      <TextField
+        id="standard-basic"
+        label="Добавить задание"
+        className={styles.InputItem}
+      />
+    </div>
 );
 
 export default InputItem;
