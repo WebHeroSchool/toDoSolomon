81a0e486417b333f72abe7f0a9823b622864a437
Соломон Захаров <solomon7and@gmail.com>
2021-06-21 18:33:43 +0500

    тудушка

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,7 +5,7 @@ import styles from '../App/App.module.css'
 const Item = ({value, isDone}) => (<span className={
   classNames({
     [styles.ItemList]: true,
-    [styles.item]: true,
+    // [styles.item]: true,
     [styles.done]: isDone
   })
 }>
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,44 @@
 import React from "react";
 import Item from "../Item/Item";
+import Checkbox from '@material-ui/core/Checkbox';
 
 const ItemList = ({items}) => (
+
   <ul>
-    {items.map(item => <li key={item.value}><Item value={item.value} isDone={item.isDone}/></li> )}
+    {items.map(item =><li key={item.value}>
+      <Checkbox /><Item value={item.value} isDone={item.isDone}/>
+    </li> )}
   </ul>
 )
 
 export default ItemList;
+
+// export default function Checkboxes() {
+//   const [checked, setChecked] = React.useState(true);
+//
+//   const handleChange = (event) => {
+//     setChecked(event.target.checked);
+//   };
+//
+//   return (
+//     <div>
+//       <li>
+//         <Checkbox
+//           checked={checked}
+//           onChange={handleChange}
+//           inputProps={{ 'aria-label': 'primary checkbox' }}
+//         />
+//       </li>
+//       <li>
+//         <Checkbox
+//           checked={checked}
+//           onChange={handleChange}
+//           inputProps={{ 'aria-label': 'primary checkbox' }}
+//         /> Привет
+//       </li>
+//     </div>
+//   );
+// }
+
+
+
