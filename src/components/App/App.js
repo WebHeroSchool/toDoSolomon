f4f8a8dabf67f63a7fa90db3e15b3d10b67cbdfe
Соломон Захаров <solomon7and@gmail.com>
2021-06-27 18:47:47 +0500

    классы

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,30 +5,63 @@ import Footer from "../Footer/Footer";
 import styles from './App.module.css';
 import classNames from "classnames"
 
-const items = [
-  {
-    value: 'Передать в футер кол-во дел которые нужно выполнить!',
-    isDone: true,
-  },
-  {
-    value: 'Передать 3 дела в Item!',
-    isDone: false,
-  },
-  {
-    value: 'Запушить в репозиторий!',
-    isDone: true,
-  },
-]
+class App extends React.Component {
+  render() {
+    const items = [
+    {
+      value: 'Передать в футер кол-во дел которые нужно выполнить!',
+      isDone: true,
+    },
+    {
+      value: 'Передать 3 дела в Item!',
+      isDone: false,
+    },
+    {
+      value: 'Запушить в репозиторий!',
+      isDone: true,
+    },
+  ]
 
-const App = () => (
-  <div className={styles.wrap}>
-    <div className={styles.content}>
-      <h1 className={styles.title}>Важные дела:</h1>
-      <InputItem />
-      <ItemList items={items} />
-      <Footer count={3}/>
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Важные дела:</h1>
+        <InputItem/>
+        <ItemList items={items}/>
+        <Footer count={3}/>
+      </div>
     </div>
-  </div>
-)
+  )
+  }
+}
+
+// const App = () => {
+//   const items = [
+//     {
+//       value: 'Передать в футер кол-во дел которые нужно выполнить!',
+//       isDone: true,
+//     },
+//     {
+//       value: 'Передать 3 дела в Item!',
+//       isDone: false,
+//     },
+//     {
+//       value: 'Запушить в репозиторий!',
+//       isDone: true,
+//     },
+//   ]
+//
+//   return (
+//     <div className={styles.wrap}>
+//       <div className={styles.content}>
+//         <h1 className={styles.title}>Важные дела:</h1>
+//         <InputItem/>
+//         <ItemList items={items}/>
+//         <Footer count={3}/>
+//       </div>
+//     </div>
+//   )
+//
+// }
 
 export default App;
