2afa4af14abf1ca255e2c1edcee962ee3679c211
Соломон Захаров <solomon7and@gmail.com>
2021-06-29 18:23:23 +0500

    перенеси items внутрь state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,32 +5,38 @@ import Footer from "../Footer/Footer";
 import styles from './App.module.css';
 
 class App extends React.Component {
-  render() {
-    const items = [
-    {
-      value: 'Передать в футер кол-во дел которые нужно выполнить!',
-      isDone: true,
-    },
-    {
-      value: 'Передать 3 дела в Item!',
-      isDone: false,
-    },
-    {
-      value: 'Запушить в репозиторий!',
-      isDone: true,
-    },
-  ]
+  state = {
+    items: [
+      {
+        value: 'Передать в футер кол-во дел которые нужно выполнить!',
+        isDone: true,
+      },
+      {
+        value: 'Передать 3 дела в Item!',
+        isDone: false,
+      },
+      {
+        value: 'Запушить в репозиторий!',
+        isDone: true,
+      },
+      {
+        value: 'Дополнительный!',
+        isDone: false,
+      },
+    ]
 
-  return (
-    <div className={styles.wrap}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Важные дела:</h1>
-        <InputItem/>
-        <ItemList items={items}/>
-        <Footer count={3}/>
+  }
+  render() {
+    return (
+      <div className={styles.wrap}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Важные дела:</h1>
+          <InputItem/>
+          <ItemList items={this.state.items}/>
+          <Footer count={this.state.items.length}/>
+        </div>
       </div>
-    </div>
-  )
+    )
   }
 }
 
