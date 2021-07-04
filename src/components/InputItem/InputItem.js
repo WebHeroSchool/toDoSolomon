cd113fb4768b7b1f2230e9c2b03d6e634cf32f19
Соломон Захаров <solomon7and@gmail.com>
2021-07-04 20:30:13 +0500

    Если мы пытаемся отправить пустое значение на добавление, то выводить ошибку

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,16 +1,49 @@
 import React from "react";
 import styles from '../App/App.module.css'
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
+class InputItem extends React.Component{
+  state = {
+    inputValue: '',
+  };
 
-const InputItem = () => (
-    <div >
-      <TextField
-        id="standard-basic"
-        label="Добавить задание"
-        className={styles.InputItem}
-      />
-    </div>
-);
+  onButtonClick = () => {
+    this.setState({
+      inputValue: '',
+    });
+
+    this.state.inputValue !== ''
+      ? this.props.onClickAdd(this.state.inputValue)
+      : console.log('error')
+      // : <TextField error id="filled-error" label="Error" defaultValue="Hello World" variant="filled" />
+  }
+
+  render() {
+    const { onClickAdd } = this.props;
+
+    return (
+      <div className={styles.Input}>
+        <TextField
+          id="standard-basic"
+          label="Добавить задание"
+          className={styles.InputItem}
+          value={this.state.inputValue}
+          onChange={e => this.setState({inputValue: e.target.value})}
+        />
+        <div className={styles.space}> </div>
+        <Button
+          variant="outlined"
+          color="primary"
+          className={styles.InputButton}
+          onClick={this.onButtonClick}
+        >
+          добавить
+        </Button>
+      </div>
+    );
+  }
+
+}
 
 export default InputItem;
