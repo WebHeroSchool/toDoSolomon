00a999fc0f0bac039d495bf16eda96b71ef8012a
Соломон Захаров <solomon7and@gmail.com>
2021-09-03 09:13:06 +0300

    Работает пагинация

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,7 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import { Octokit } from '@octokit/rest';
 import classNames from "classnames";
 import styles from '../About/About.module.css'
+import arrow from '../About/arrow.png'
 
 const octokit = new Octokit();
 
@@ -10,7 +11,27 @@ class About extends React.Component {
   state = {
     isLoading: true,
     repoList: [],
+    infoAboutUser: [],
+    isErrorUser: false,
+    isErrorRepo: false,
+    errorMessage: '',
+    firstRepo: 0,
+    lastRepo: 4
   }
+
+  prevPage = () => {
+    this.setState({
+        firstRepo: this.state.firstRepo - 4,
+        lastRepo: this.state.lastRepo - 4
+    });
+};
+
+  nextPage = () => {
+    this.setState({
+        firstRepo: this.state.firstRepo + 4,
+        lastRepo: this.state.lastRepo + 4
+    });
+};
 
   componentDidMount() {
     octokit.repos.listForUser({
@@ -47,7 +68,8 @@ class About extends React.Component {
   }
 
   render() {
-    const { isLoading, repoList, infoUser, errorMessage, isError } = this.state;
+    const { isLoading, repoList, infoUser, errorMessage, isError, firstRepo, lastRepo } = this.state;
+    const repoListPage = repoList.slice(firstRepo, lastRepo);
 
     return (
         <div>
@@ -95,48 +117,60 @@ class About extends React.Component {
                   </div>
 
                   <div className={styles.content}>
-                    <h3>Мои работы:</h3>
-                    <ol>
-                      {repoList === undefined
-                        ? 'Информация не найдена'
-                        : repoList.map((repo) => (
-                          <li className={styles.repoList} key={repo.id}>
-                            <a className={styles.repoLinks} target="_blank" rel = "noreferrer" href={repo.html_url}>
-                              <span>{repo.name}</span>
-                              <span>
-                                {repo.description}
-                              </span>
-                              <div className={styles.repo__info}>
-                                <span className={
-                                  repo.language === null
-                                   ? repo.language = 'CSS'
-                                    :
-                                  classNames({
-                                    [styles.language]: true,
-                                    [styles.html]: repo.language === 'HTML',
-                                    [styles.css]: repo.language === 'CSS',
-                                    [styles.js]: repo.language === 'JavaScript',
-                                  })}>
-                                {repo.language}
+                    <div>
+                      <div className={styles.repoHeader}>
+                        <h3>Мои работы на github.com:</h3>
+                        <div className={styles.arrows}>
+                          <button onClick={this.prevPage} disabled={firstRepo < 4}>
+                              <img className={styles.arrow_left} src={arrow} alt='scroll-left'/>
+                          </button>
+                          <button onClick={this.nextPage} disabled={repoList.length <= lastRepo}>
+                            <img className={styles.arrow_right} src={arrow} alt='scroll-right'/>
+                          </button>
+                         </div>
+                      </div>
+                      <ol>
+                        {repoList === undefined
+                          ? 'Информация не найдена'
+                          : repoListPage.map((repo) => (
+                            <li className={styles.repoList} key={repo.id}>
+                              <a className={styles.repoLinks} target="_blank" rel = "noreferrer" href={repo.html_url}>
+                                <span>{repo.name}</span>
+                                <span>
+                                  {repo.description}
                                 </span>
-                                <span
-                                  className={styles.star}>{repo.stargazers_count}</span>
-                                <span
-                                  className={styles.fork}>{repo.forks_count}</span>
-                                <span> Updated on {new Date(repo.updated_at).toLocaleString('en-US', {
-                                  day: 'numeric',
-                                  month: 'short',
-                                  year: 'numeric',
-                                })}</span>
-                              </div>
-                            </a>
-                            <a className={styles.repoHold}
-                               target="_blank" rel = "noreferrer"
-                               href={repo.name === 'todoSolomon' ? `https://todo-solomon-cyan.vercel.app/` : `https://Solomon7and7.github.io/${repo.name}`}>
-                              холдинг</a>
-                          </li>
-                        ))}
-                    </ol>
+                                <div className={styles.repo__info}>
+                                  <span className={
+                                    repo.language === null
+                                    ? repo.language = 'CSS'
+                                      :
+                                    classNames({
+                                      [styles.language]: true,
+                                      [styles.html]: repo.language === 'HTML',
+                                      [styles.css]: repo.language === 'CSS',
+                                      [styles.js]: repo.language === 'JavaScript',
+                                    })}>
+                                  {repo.language}
+                                  </span>
+                                  <span
+                                    className={styles.star}>{repo.stargazers_count}</span>
+                                  <span
+                                    className={styles.fork}>{repo.forks_count}</span>
+                                  <span> Updated on {new Date(repo.updated_at).toLocaleString('en-US', {
+                                    day: 'numeric',
+                                    month: 'short',
+                                    year: 'numeric',
+                                  })}</span>
+                                </div>
+                              </a>
+                              <a className={styles.repoHold}
+                                target="_blank" rel = "noreferrer"
+                                href={repo.name === 'todoSolomon' ? `https://todo-solomon-ee6rfoy8i-todosolomon3.vercel.app/` : `https://Solomon7and7.github.io/${repo.name}`}>
+                                холдинг</a>
+                            </li>
+                          ))}
+                      </ol>
+                    </div>
                   </div>
                 </div>
               }
